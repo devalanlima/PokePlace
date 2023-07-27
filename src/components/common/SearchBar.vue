06fd05050e31745d1d525a13d909fad4f3fbb9f6
Alan Lima <dev.alanlima@gmail.com>
2023-07-27 10:40:27 -0300

    create / SearchBar.vue component

---
+++ src/components/common/SearchBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-full relative">
+        <input class="bg-transparent outline outline-2 outline-MainBlue p-3 rounded-full w-full pl-14" placeholder="Ex.: Pikachu"
+            type="text" name="" id="search-bar">
+        <label class="absolute left-0 top-0" for="search-bar">
+            <IconSearch class=" -ml-[1px]" />
+        </label>
+    </div>
+</template>
+
+<script setup>
+import IconSearch from '../icons/IconSearch.vue';
+
+</script>
