a32d56831404f43482f5909c2d8728c2b2cf89d7
Alan Lima <dev.alanlima@gmail.com>
2023-07-31 13:26:33 -0300

    create / SupertypeFilter.vue component

---
+++ src/components/SupertypeFilter.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav>
+        <SelectInput button-name="Supertype" class="w-full">
+            <ul class="w-full">
+                <li v-for="supertype in allSuperTypes" :key="supertype">
+                    <input class="peer sr-only" name="selectSupertype" type="radio" :id="supertype" :value="supertype" v-model="searchFilters.supertype">
+                    <label  class="hover:bg-blue-900 bg-opacity-25 w-full block px-5 py-3 peer-checked:bg-blue-950 cursor-pointer" :for="supertype">{{ supertype }}</label>
+                </li>
+            </ul>
+        </SelectInput>
+    </nav>
+</template>
+
+<script setup>
+import SelectInput from './ui/SelectInput.vue';
+import { useSearchFilters } from '../stores/SearchFilter';
+
+const searchFilters = useSearchFilters()
+import { ref} from 'vue'
+
+const allSuperTypes = ref([
+    "Energy",
+    "Pokémon",
+    "Trainer"
+])
+</script>
