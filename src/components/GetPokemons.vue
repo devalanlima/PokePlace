4f92b253d00455cb2e481561289ecf58e927768c
Alan Lima <dev.alanlima@gmail.com>
2023-08-09 12:37:44 -0300

    refactor / changed searchFilter.supertype to run conditionally

--- src/components/GetPokemons.vue
+++ src/components/GetPokemons.vue
@@ -1,8 +1,7 @@
 <template>
     <template v-for="pokemon in dataApi.data" :key="pokemon.id">
         <div class="p-3 rounded-xl flex relative overflow-hidden"
-        :class="pokemon.types.length > 1 ? defineBackGround(pokemon.types)[1] : defineBackGround(pokemon.types)"
-        >
+            :class="pokemon.types && pokemon.types.length > 1 ? defineBackGround(pokemon.types)[1] : defineBackGround(pokemon.types)">
             <div class="z-10 flex flex-col gap-3">
                 <div v-if="pokemon.images.small !== 'https://images.pokemontcg.io/ecard2/67.png'">
                     <CardEffect>
@@ -15,7 +14,9 @@
                     <p>234 of 540</p>
                 </div>
             </div>
-            <div class="absolute w-[240%] h-[240%] top-0 left-0 rotate-45" :class="defineBackGround(pokemon.types)[0]"></div>
+            <div class="absolute w-[240%] h-[240%] top-0 left-0 rotate-45"
+                :class="pokemon.types ? defineBackGround(pokemon.types)[0] : 'bg-MainWhite'">
+            </div>
         </div>
     </template>
 </template>
@@ -39,7 +40,7 @@ const api = axios.create({
         subtypes:"${searchFilters.subtype}"
         supertype:"${searchFilters.supertype}"
         rarity:"${searchFilters.rarity}"
-        ${searchFilters.pokemonType}
+        ${searchFilters.supertype === "Pokémon" ? searchFilters.pokemonType.join(" ") : searchFilters.pokemonType}
         `,
     }
 })
@@ -62,45 +63,50 @@ const dataApi = await getPokemons(props.currentPage)
 
 const defineBackGround = (pokemonType) => {
     let tempBg = []
+    if (pokemonType && pokemonType.length > 0) {
         pokemonType.forEach(element => {
-        switch (element) {
-            case 'Water':
-                tempBg.unshift('bg-WaterBlue')
-                break;
-            case 'Lightning':
-                tempBg.unshift('bg-LightningYellow')
-                break;
-            case 'Psychic':
-                tempBg.unshift('bg-PsychicPurple')
-                break;
-            case 'Fighting':
-                tempBg.unshift('bg-FightingBronw')
-                break;
-            case 'Darkness':
-                tempBg.unshift('bg-DarknessBlue')
-                break;
-            case 'Dragon':
-                tempBg.unshift('bg-DragonYellow')
-                break;
-            case 'Colorless':
-                tempBg.unshift('bg-ColorlessWhite')
-                break;
-            case 'Fire':
-                tempBg.unshift('bg-FireRed')
-                break;
-            case 'Grass':
-                tempBg.unshift('bg-GrassGreen')
-                break;
-            case 'Metal':
-                tempBg.unshift('bg-MetalGray')
-                break;
-            case 'Fairy':
-                tempBg.unshift('bg-FairyPink')
-                break;
-            default:
-                break;
-        }
-    }); 
+            switch (element) {
+                case 'Water':
+                    tempBg.unshift('bg-WaterBlue')
+                    break;
+                case 'Lightning':
+                    tempBg.unshift('bg-LightningYellow')
+                    break;
+                case 'Psychic':
+                    tempBg.unshift('bg-PsychicPurple')
+                    break;
+                case 'Fighting':
+                    tempBg.unshift('bg-FightingBronw')
+                    break;
+                case 'Darkness':
+                    tempBg.unshift('bg-DarknessBlue')
+                    break;
+                case 'Dragon':
+                    tempBg.unshift('bg-DragonYellow')
+                    break;
+                case 'Colorless':
+                    tempBg.unshift('bg-ColorlessWhite')
+                    break;
+                case 'Fire':
+                    tempBg.unshift('bg-FireRed')
+                    break;
+                case 'Grass':
+                    tempBg.unshift('bg-GrassGreen')
+                    break;
+                case 'Metal':
+                    tempBg.unshift('bg-MetalGray')
+                    break;
+                case 'Fairy':
+                    tempBg.unshift('bg-FairyPink')
+                    break;
+                default:
+                    tempBg.unshift('bg-MainWhite')
+                    break;
+            }
+        });
+    } else {
+        tempBg.unshift('bg-MainWhite')
+    }
     return tempBg
 }
 </script>
