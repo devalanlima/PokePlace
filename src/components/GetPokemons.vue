98690bd7ad979ece196be8986f00027a0752abfd
Alan Lima <dev.alanlima@gmail.com>
2023-08-14 12:29:31 -0300

    add / response to no cards matches with filter

--- src/components/GetPokemons.vue
+++ src/components/GetPokemons.vue
@@ -1,22 +1,30 @@
 <template>
-    <template v-for="pokemon in dataApi.data" :key="pokemon.id">
-        <div class="p-3 rounded-xl flex relative overflow-hidden"
-            :class="pokemon.types && pokemon.types.length > 1 ? defineBackGround(pokemon.types)[1] : defineBackGround(pokemon.types)">
-            <div class="z-10 flex flex-col gap-3">
-                <div v-if="pokemon.images.small !== 'https://images.pokemontcg.io/ecard2/67.png'">
-                    <CardEffect>
-                        <img :src="pokemon.images.small" :alt="pokemon.name">
-                    </CardEffect>
+    <template v-if="dataApi">
+        <template v-for="pokemon in dataApi.data" :key="pokemon.id">
+            <div class="p-3 rounded-xl flex relative overflow-hidden"
+                :class="pokemon.types && pokemon.types.length > 1 ? defineBackGround(pokemon.types)[1] : defineBackGround(pokemon.types)">
+                <div class="z-10 flex flex-col gap-3">
+                    <div v-if="pokemon.images.small !== 'https://images.pokemontcg.io/ecard2/67.png'">
+                        <CardEffect>
+                            <img :src="pokemon.images.small" :alt="pokemon.name">
+                        </CardEffect>
+                    </div>
+                    <div class="w-full h-[1px] bg-SecondaryBlue block"></div>
+                    <div class="flex justify-between text-SecondaryBlue font-semibold">
+                        <p>0.000054 ETH</p>
+                        <p>234 of 540</p>
+                    </div>
                 </div>
-                <div class="w-full h-[1px] bg-SecondaryBlue block"></div>
-                <div class="flex justify-between text-SecondaryBlue font-semibold">
-                    <p>0.000054 ETH</p>
-                    <p>234 of 540</p>
+                <div class="absolute w-[240%] h-[240%] top-0 left-0 rotate-45"
+                    :class="pokemon.types ? defineBackGround(pokemon.types)[0] : 'bg-MainWhite'">
                 </div>
             </div>
-            <div class="absolute w-[240%] h-[240%] top-0 left-0 rotate-45"
-                :class="pokemon.types ? defineBackGround(pokemon.types)[0] : 'bg-MainWhite'">
-            </div>
+        </template>
+    </template>
+    <template v-else>
+        <div class="flex flex-col justify-center text-center gap-5 max-w-[310px]">
+            <img src="/No_Matches.png" alt="magnifying glass with abstract symbols">
+            <p class="text-3xl text-MainBlue font-bold">No more cards matches your filter</p>
         </div>
     </template>
 </template>
@@ -49,11 +57,19 @@ const props = defineProps({
     currentPage: { Type: Number, default: 1 }
 })
 
+const emit = defineEmits(['update:hasItens',])
+
 const getPokemons = async (page) => {
     try {
         const response = api.get(`/cards?orderBy=${searchFilters.order}&page=${page}`)
         const dataApi = (await response).data
-        return dataApi
+        if (dataApi.data.length !== 0) {
+            emit('update:hasItens', true)
+            return dataApi
+        } else{
+            emit('update:hasItens', false)
+            return false
+        }
     } catch (error) {
         console.log(error);
     }
@@ -109,4 +125,5 @@ const defineBackGround = (pokemonType) => {
     }
     return tempBg
 }
+
 </script>
