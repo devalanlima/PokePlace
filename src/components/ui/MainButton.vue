3551d17f8d2438e362183f408a7a50117ff56906
Alan Lima <dev.alanlima@gmail.com>
2023-08-14 12:27:18 -0300

    add / disabled option

--- src/components/ui/MainButton.vue
+++ src/components/ui/MainButton.vue
@@ -1,12 +1,16 @@
 <template>
-    <button :class="['px-5 py-4 outline outline-1 outline-MainBlue rounded-full whitespace-nowrap', {'bg-MainBlue': props.haveBackGround}]"
-    >{{ props.buttonName }}</button>
+    <button :class="['px-5 py-4 outline outline-1 outline-MainBlue rounded-full whitespace-nowrap', {'bg-MainBlue': props.haveBackGround}, {'bg-slate-500 cursor-not-allowed outline-slate-700': props.disabled === true}]"
+    :disabled="disabled"
+    >
+        {{ props.buttonName }}
+    </button>
 </template>
 
 <script setup>
 const props = defineProps({
     buttonName:{Type:String, default: 'Button'},
-    haveBackGround:{type: Boolean, default: true}
+    haveBackGround:{type: Boolean, default: true},
+    disabled:{type: Boolean, default: false}
 })
 
 </script>
